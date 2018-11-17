406921ca2f08420e8449ff7ffade46dc2d58b458
Andrew Galushka <nixdeveloperios@gmail.com>
2018-11-17 12:09:22 +0200

    Added design class

---
+++ Green-Space/Green-Space/Design.swift
@@ -0,0 +1,19 @@
+//
+//  Design.swift
+//  Green-Space
+//
+//  Created by Galushka on 11/17/18.
+//  Copyright © 2018 Galushka. All rights reserved.
+//
+
+import UIKit
+
+class Design {
+    
+    class Colors {
+        
+        static var background: UIColor {
+            return UIColor(red:0.09, green:0.11, blue:0.08, alpha:1.0)
+        }
+    }
+}
