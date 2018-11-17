167319c3bb724883e584d337e416fea677eb243c
Andrew Galushka <nixdeveloperios@gmail.com>
2018-11-17 13:05:55 +0200

    starting work on drop animation, GameVC moved to separated storyboard

--- Green-Space/Green-Space/GameViewController/FallingApplesGameView.swift
+++ Green-Space/Green-Space/GameViewController/FallingApplesGameView.swift
@@ -14,6 +14,7 @@ class FallingApplesGameView: UIView {
     let initailApearnceInterval = 1.0
     
     var timer: Timer?
+    var apples = [CALayer]()
     
     func start() {
         timer = Timer(timeInterval: initailApearnceInterval, target: self, selector: #selector(timerTickActionHandler), userInfo: nil, repeats: true)
@@ -34,6 +35,20 @@ class FallingApplesGameView: UIView {
     }
     
     func addApple() {
+        let origin = CGPoint(x: randXCoordinate(forAppleSize: appleSize),
+                             y: -(appleSize.height / 2))
+        let cornerRadius = max(appleSize.width, appleSize.height) / 2
+        let bkColor = UIColor.red.cgColor
+//        let animationEndPoint =
+        
+        let appleLayer = CALayer()
+        appleLayer.frame = CGRect(x: origin.x, y: origin.y, width: appleSize.width, height: appleSize.height)
+        appleLayer.cornerRadius = cornerRadius
+        appleLayer.backgroundColor = bkColor
+        
+        apples.append(appleLayer)
+        layer.addSublayer(appleLayer)
+//        appleLayer.add( dropAnimation(startPoint: origin, endPoint: <#T##CGPoint#>), forKey: <#T##String?#>)
     }
     
     func randXCoordinate(forAppleSize: CGSize) -> CGFloat {
@@ -42,5 +57,14 @@ class FallingApplesGameView: UIView {
         let randXCoordinate = CGFloat.random(in: 0...(contentViewWidth - appleWidth))
         
         return randXCoordinate
+    }
+    
+    func dropAnimation(startPoint: CGPoint, endPoint: CGPoint) -> CAAnimation {
+        let dropAnimation = CASpringAnimation(keyPath: "position")
+        dropAnimation.fromValue = startPoint
+        dropAnimation.toValue = endPoint
+        dropAnimation.duration = 3.0
+        
+        return dropAnimation
     }
 }
