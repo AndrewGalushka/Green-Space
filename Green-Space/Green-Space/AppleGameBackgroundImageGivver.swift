91592fb793e7f9b09a0a929ff3b69dbd2b2c5426
Andrew Galushka <nixdeveloperios@gmail.com>
2018-11-17 16:05:59 +0200

    Protocol for image getting

---
+++ Green-Space/Green-Space/AppleGameBackgroundImageGivver.swift
@@ -0,0 +1,14 @@
+//
+//  AppleGameBackgroundImageGivver.swift
+//  Green-Space
+//
+//  Created by Galushka on 11/17/18.
+//  Copyright © 2018 Galushka. All rights reserved.
+//
+
+import Foundation
+
+protocol AppleGameBackgroundImageGivver: class {
+    func imageForSuccess()
+    func imageForFailure()
+}
